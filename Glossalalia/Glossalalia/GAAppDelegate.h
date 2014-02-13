64b15f2401a7b3708bfb89bd844957dce26e9a4b
Rupert Deese <rdeese@hmc.edu>
2014-02-13 15:47:39 -0800

    Added empty Xcode project

---
+++ Glossalalia/Glossalalia/GAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GAAppDelegate.h
+//  Glossalalia
+//
+//  Created by Rupert Deese on 2/13/14.
+//  Copyright (c) 2014 Rupert Deese. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Glossalalia/Glossalalia/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Glossalalia
+//
+//  Created by Rupert Deese on 2/13/14.
+//  Copyright (c) 2014 Rupert Deese. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GAAppDelegate class]));
+    }
+}
