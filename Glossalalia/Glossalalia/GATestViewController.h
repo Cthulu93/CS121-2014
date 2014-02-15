3ef3e36cda65a4cb5fcd23bea9eecb406c4e109d
Rupert Deese <rdeese@hmc.edu>
2014-02-15 00:14:58 -0800

    phone to phone communication works!
    button on one screen changes text on the other screen and vice versa.
    victory!

--- Glossalalia/Glossalalia/GATestViewController.h
+++ Glossalalia/Glossalalia/GATestViewController.h
@@ -12,12 +12,14 @@
 @interface GATestViewController : UIViewController <GKMatchmakerViewControllerDelegate, GKMatchDelegate>
 
 @property UIButton *matchmakeButton;
-//@property UILabel *
+@property UILabel *gameStatus;
+@property UIButton *gameButton;
 @property GKMatch *theMatch;
 @property BOOL matchOn;
 
 - (void) requestMatch;
 - (void) authenticatedPlayer: (GKLocalPlayer*) lp;
 - (void) match:(GKMatch *)match didReceiveData:(NSData *)data fromPlayer:(NSString *)playerID;
+- (void) sendMessage;
 
 @end
--- Glossalalia/Glossalalia/GATestViewController.m
+++ Glossalalia/Glossalalia/GATestViewController.m
@@ -28,6 +28,16 @@
         [self.view addSubview:_matchmakeButton];
         [_matchmakeButton addTarget:self action:@selector(requestMatch) forControlEvents:UIControlEventTouchUpInside];
         
+        _gameStatus = [[UILabel alloc] initWithFrame:CGRectMake(0.05*_fWidth, 0.2*_fHeight, 0.9*_fWidth, 0.2*_fHeight)];
+        [_gameStatus setBackgroundColor:[UIColor magentaColor]];
+        [self.view addSubview:_gameStatus];
+        
+        _gameButton = [[UIButton alloc] initWithFrame:CGRectMake(0.05*_fWidth, 0.45*_fHeight, 0.9*_fWidth, 0.1*_fHeight)];
+        
+        [_gameButton setTitle:@"Push them!" forState:UIControlStateNormal];
+        [_gameButton setBackgroundColor:[UIColor purpleColor]];
+        [_gameButton addTarget:self action:@selector(sendMessage) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:_gameButton];
         
         _matchOn = false;
         [self authenticateLocalPlayer];
@@ -62,18 +72,27 @@
 }
 
 - (void) requestMatch {
-    GKMatchRequest *request = [[GKMatchRequest alloc] init];
-    request.minPlayers = 2;
-    request.maxPlayers = 2;
-    
-    GKMatchmakerViewController *mmvc = [[GKMatchmakerViewController alloc] initWithMatchRequest:request];
-    mmvc.matchmakerDelegate = self;
-    
-    [self presentViewController:mmvc animated:YES completion:nil];
+    if (!_matchOn) {
+        GKMatchRequest *request = [[GKMatchRequest alloc] init];
+        request.minPlayers = 2;
+        request.maxPlayers = 2;
+        
+        GKMatchmakerViewController *mmvc = [[GKMatchmakerViewController alloc] initWithMatchRequest:request];
+        mmvc.matchmakerDelegate = self;
+        
+        [self presentViewController:mmvc animated:YES completion:nil];
+    }
 }
 
 - (void) authenticatedPlayer: (GKLocalPlayer*) lp {
     NSLog(@"%@ has been authenticated!", [lp displayName]);
+}
+
+- (void) sendMessage {
+    if (_matchOn) {
+        [_theMatch sendDataToAllPlayers:[@"Push now!" dataUsingEncoding:NSUTF8StringEncoding] withDataMode:GKMatchSendDataUnreliable error:nil];
+    }
+    [_gameStatus setText:@"Wait!"];
 }
 
 - (void)matchmakerViewControllerWasCancelled:(GKMatchmakerViewController *)viewController
@@ -102,7 +121,7 @@
 }
 
 - (void) match:(GKMatch *)match didReceiveData:(NSData *)data fromPlayer:(NSString *)playerID {
-    
+    [_gameStatus setText:[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]];
 }
 
 - (void)viewDidLoad
